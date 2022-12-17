c57505e33ff6a0bd9768d5533f7fa9bff186da8a
GaneshXworkz <ganeshkp.xworkz@gmail.com>
2022-12-17 22:55:13 +0530

    new

---
+++ JavaMemory/ToStringMethod.java
@@ -0,0 +1,14 @@
+package com.Xworkz.JavaMemory;
+
+public class ToStringMethod {
+
+	public static void main(String[] args) {
+		String str="Raja nanna Raja";
+		
+		System.out.println(str);
+		
+		Beaches beaches=new Beaches();
+		
+		
+	}
+}
