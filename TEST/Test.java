533b7cad6b3b19611dee9f956611ef35a7e09b79
GaneshXworkz <ganeshkp.xworkz@gmail.com>
2023-01-10 19:36:31 +0530

    new

---
+++ TEST/Test.java
@@ -0,0 +1,17 @@
+package com.xworkz.Collection.TEST;
+
+public class Test {
+	
+ public static void main(String[] args) {
+	
+	 String sen="1234";
+	 int a=Integer.parseInt(sen);
+	 System.out.println(a);
+	 
+	 
+	
+}
+	
+	
+
+}
